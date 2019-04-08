cb20e9ac3426b4a74ff0accf848c3b6780574bf9
Chris Ruffalo <chris.ruffalo@gmail.com>
2019-04-08 13:59:52 -0400

    changed the way the import works, again

--- web/app/js/gudgeon/query-tester.js
+++ web/app/js/gudgeon/query-tester.js
@@ -13,7 +13,7 @@ import {
   GridItem,
   Grid
 } from "@patternfly/react-core";
-import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import SyntaxHighlighter from 'react-syntax-highlighter';
 import { googlecode } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import gudgeonStyles from '../../css/gudgeon-app.css';
 import { css } from '@patternfly/react-styles';
